4598fb0b85f9e2b62c4d483896be948b81d34950
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2015-03-30 23:36:35 +0200

    Added gpxparser proyect

---
+++ gpx-parser/src/main/java/org/alternativevision/gpx/beans/TrackSeg.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.alternativevision.gpx.beans;
+
+/**
+ *
+ * @author Fer
+ */
+public class TrackSeg {
+    
+}
